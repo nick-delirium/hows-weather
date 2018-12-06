bac28fb3c18cc299e0e4bc0c12f539b34c4ca1a0
Nikita Melnikov <sylenien@gmail.com>
2018-12-06 16:25:26 +0300

    static for heroku

---
+++ build/precache-manifest.be043ca98df7241f1c42e28505f96010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6759500797942ba5f178",
+    "url": "/static/js/main.67595007.chunk.js"
+  },
+  {
+    "revision": "ba50f34af62e7110e3ab",
+    "url": "/static/js/1.ba50f34a.chunk.js"
+  },
+  {
+    "revision": "6759500797942ba5f178",
+    "url": "/static/css/main.d49e457b.chunk.css"
+  },
+  {
+    "revision": "89724454edc98d81d2257e6ad84ed5f3",
+    "url": "/index.html"
+  }
+];
