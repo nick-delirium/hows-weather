568e8df51b57b01ded9ceb6632c55cf8694e4b9f
Nikita Melnikov <sylenien@gmail.com>
2018-12-06 17:17:04 +0300

    throw devtools from prod

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,7 @@ import reducer from "./reducers";
 
 const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(thunk),
-    typeof window === "object" &&
-    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : f => f
-  )
+  applyMiddleware(thunk)
 );
 
 export default store;
